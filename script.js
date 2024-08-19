e783de3895d3a2c3868a53ed2467371e7242dbc2
Ivan Joshua Tomazar <ijstomazar@gmail.com>
2024-08-19 17:11:11 +0800

    graphic changes

--- script.js
+++ script.js
@@ -4,3 +4,51 @@ function toggleMenu(){
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+function navigateTo(url) {
+    window.location.href = url;
+}
+
+function scrollTo(id) {
+    var element = document.getElementById(id);
+    
+    if (element) {
+        element.scrollIntoView({
+            behavior: 'smooth', // Smooth scrolling
+            block: 'center',      // Align the element to the top of the view
+            inline:'nearest'
+        });
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var typingElement = document.querySelector('.typing');
+
+    function isInViewport(element) {
+        var rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    function checkTypingAnimation() {
+        if (isInViewport(typingElement)) {
+            typingElement.style.animation = 
+            'typing 1.5s steps(20, end), blink-caret .75s step-end infinite';
+            typingElement.style.color = 'white';
+        }
+        else{
+            typingElement.style.animation = 'none';
+            typingElement.style.color = 'transparent';
+        }
+    }
+
+    // Check on scroll and on page load
+    window.addEventListener('scroll', checkTypingAnimation);
+    window.addEventListener('resize', checkTypingAnimation);
+    checkTypingAnimation(); // Initial check
+});
+
